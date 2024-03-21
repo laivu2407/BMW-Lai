f7ece11c0a10781e43a61c1e562bb2f51d9edd9d
laivu2407 <153612385+laivu2407@users.noreply.github.com>
2024-03-21 20:28:57 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import AddBookForm from './AddBookForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Library Management System</h1>
+      <AddBookForm />
+    </div>
+  );
+}
+
+export default App;
